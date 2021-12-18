bdffaad636761dfd788a156236d50bb3751ec1c3
daoud02 <94905346+daoud02@users.noreply.github.com>
2021-12-18 14:36:49 +0100

    django

--- All/admin.py
+++ All/admin.py
@@ -1,9 +1,1 @@
 from django.contrib import admin
-from .models import *
-
-admin.site.register(Customer)
-admin.site.register(Products)
-admin.site.register(Order)
-admin.site.register(Items)
-admin.site.register(ShippingAddress)
-admin.site.register(Message)
--- All/urls.py
+++ All/urls.py
@@ -7,14 +7,7 @@ from django.views.static import serve
 urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-    path('' , views.indexx , name='index'),
-    path('boutique' , views.boutique , name='boutique'),
-    path('amour' , views.amour , name='amour'),
-    path('anniversaire' , views.anniversaire , name='anniversaire'),
-    path('naissance' , views.naissance , name='naissance'),
-    path('remerciement' , views.remerciement , name='remerciement'),
-    path('product/<int:id_test>' , views.product , name='product'),
-    path('cart' , views.cart , name="cart"),
-    path('about-us', views.about , name='about-us'),
-    path('contact-us', views.Contact_Us , name='contact_us'),
+    path('' , views.index , name='index'),
+    path('yesterday' , views.yesterday , name='yesterday'),
+    path('tomorrow' , views.tomorrow , name='tomorrow'),
 ]
